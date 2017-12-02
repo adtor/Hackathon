bef57cdf8704057d9a23a6ec924f259aa47306e3
adtor <34197876+adtor@users.noreply.github.com>
2017-12-03 04:50:05 +0530

    Initial commit

---
+++ app/src/main/java/shubham_serva/hackathon/MainActivity.java
@@ -0,0 +1,17 @@
+package shubham_serva.hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button skilled,unskilled;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
